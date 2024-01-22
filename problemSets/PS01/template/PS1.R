60753be85697a9add8f0245b4ce1c454bdc5ef40
Jeffrey Ziegler <jeffreymziegler@gmail.com>
2024-01-22 14:53:05 +0000

    Add PS01

---
+++ problemSets/PS01/template/PS1.R
@@ -0,0 +1,53 @@
+#####################
+# load libraries
+# set wd
+# clear global .envir
+#####################
+
+# remove objects
+rm(list=ls())
+# detach all libraries
+detachAllPackages <- function() {
+  basic.packages <- c("package:stats", "package:graphics", "package:grDevices", "package:utils", "package:datasets", "package:methods", "package:base")
+  package.list <- search()[ifelse(unlist(gregexpr("package:", search()))==1, TRUE, FALSE)]
+  package.list <- setdiff(package.list, basic.packages)
+  if (length(package.list)>0)  for (package in package.list) detach(package,  character.only=TRUE)
+}
+detachAllPackages()
+
+# load libraries
+pkgTest <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[,  "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg,  dependencies = TRUE)
+  sapply(pkg,  require,  character.only = TRUE)
+}
+
+# here is where you load any necessary packages
+# ex: stringr
+# lapply(c("stringr"),  pkgTest)
+
+lapply(c(),  pkgTest)
+
+# set wd for current folder
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+
+#####################
+# Problem 1
+#####################
+
+set.seed(123)
+# create empirical distribution of observed data
+ECDF <- ecdf(data)
+empiricalCDF <- ECDF(data)
+# generate test statistic
+D <- max(abs(empiricalCDF - pnorm(data)))
+
+#####################
+# Problem 2
+#####################
+
+set.seed (123)
+data <- data.frame(x = runif(200, 1, 10))
+data$y <- 0 + 2.75*data$x + rnorm(200, 0, 1.5)
